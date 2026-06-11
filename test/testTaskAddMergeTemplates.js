0b23070cfc475b6a1ca27d5d7589fb852e94b698
Trialpearswiki <125901727+Trialpearswiki@users.noreply.github.com>
2026-06-11 16:52:26 +0200

    Fix lint issues

--- test/testTaskAddMergeTemplates.js
+++ test/testTaskAddMergeTemplates.js
@@ -7,7 +7,6 @@ import Discussion from "../xfdcloser-src/Models/Discussion";
 import Venue from "../xfdcloser-src/Venue";
 import Result from "../xfdcloser-src/Models/Result";
 import Options from "../xfdcloser-src/Models/Options";
-import config from "../xfdcloser-src/config";
 import { dmyDateString } from "../xfdcloser-src/util";
 
 // Mock widgets
